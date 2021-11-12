c1951ca442efd3202b39a9f5595a97f833e78496
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2021-11-12 17:47:08 +1100

    add isREAD option

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
 let myLibrary = [];
 
 function Book(title, author, page){
-    this.title = title
-    this.author = author
-    this.pages = page
-    this.isRead = false
+    this.title = title.value
+    this.author = author.value
+    this.pages = page.value
     }
 
 
-function addBookToLibrary(book){
-    myLibrary.push(book)
-}
+// function addBookToLibrary(book){
+//     myLibrary.push(book)
+// }
 
-function displayBooks(){
-    for (book in myLibrary){
-
-    }
-}
-
-
+// function displayBooks(){
+//     for (book in myLibrary){
+//         console.log(book)
+//     }
+// }
 
 
-const harryPotter = new Book('Harry Potter', 'JK Rolling', '250')
-
-const book1 = new Book('ASDD', 'Mark', '300')
 
 
-addBookToLibrary(harryPotter)
-addBookToLibrary(book1)
+const submit = document.getElementById('submit')
+const table = document.querySelector('.table')
+const title = document.getElementById('title')
+const author = document.getElementById('author')
+const page = document.getElementById('page')
+const tableBody = document.querySelector('.tbody')
+// const datatitle = document.querySelector('[date-title')
+// const dataauthor = document.querySelector('[data-author]')
+// const datapage = document.querySelector('[data-page]')
 
+const readBtn = document.querySelector('[data-read]')
+readBtn.addEventListener('click',()=>{
+    if (readBtn.textContent === 'No'){
+        readBtn.textContent = 'Yes'}
+    else {
+        readBtn.textContent = 'No'
+    } 
+})
+
+
+// submit.addEventListener('click', ()=>{
+// const newBook = new Book(title, author, page);
+// console.log(newBook)
+// })
